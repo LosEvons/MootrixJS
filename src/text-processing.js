c7e287eac49844e359fe4fce413767feeca1f7bc
LosEvons <emil.malk@gmail.com>
2023-06-19 20:58:58 +0300

    Implemented rudimentary parsing.

--- src/text-processing.js
+++ src/text-processing.js
@@ -3,7 +3,9 @@ async function readTextFile(text_file) {
 	let text = await text_file.files[0].text();
 
 	// remove unnecessary characters
-	text = text.replace(/[^0-9a-z]/gi, "");
-	let tokens = text.split(" ");	
+	text = text.replace(/[^0-9a-zA-Z\u00f6\u00e4\u00d6\u00c4-]/gi, " ");
+	text = text.trim().replace(/\s{2,}/g, ' ');
+	text = text.toLowerCase();
+	let tokens = text.split(" ");
 	console.log(tokens);
 }
